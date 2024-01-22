5867cbdb949736f5e0bfd428ce542b3d855a9f65
01110011r <samandarrasulov934@gmail.com>
2024-01-23 01:15:12 +0500

    modify

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,37 @@ import fastify from "fastify";
 
 
 const main=():void=>{
-    const server=fastify();
+    // const server=fastify();
+    //
+    // server.listen(5000,(err:Error | null, address:string):void=>{
+    //     if(err){
+    //         console.error(err);
+    //         process.exit(1);
+    //     };
+    //     console.log("server listening at "+ address);
+    // });
 
-    server.listen(5000,(err:Error | null, address:string):void=>{
-        if(err){
-            console.error(err);
-            process.exit(1);
-        };
-        console.log("server listening at "+ address);
-    });
+
+// Import the framework and instantiate it
+    import Fastify from 'fastify'
+    const fastify = Fastify({
+        logger: true
+    })
+
+// Declare a route
+    fastify.get('/', async function handler (request, reply) {
+        return { hello: 'world' }
+    })
+
+// Run the server!
+    try {
+        await fastify.listen({ port: 3000 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+
 };
 
 main();
+
